4ed1769256cc7471228939d9353e41ec9848eebe
janpoo <janpoo@163.com>
2013-09-24 18:28:30 +0800

    import project

---
+++ VoteCracker/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(506, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "测试投票";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(329, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(66, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "投票数";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(13, 129);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(568, 268);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(282, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "代理";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(172, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "批量投票";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(329, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 397);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ VoteCracker/Form1.cs
@@ -0,0 +1,398 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net.Sockets;
+using System.Net;
+
+using System.IO;
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+
+        string[] historyArr = null;
+
+        //人员:IP:Port:MSG
+        string[] proxyArr = null;
+        string[] manArr = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+          
+        }
+
+       
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string[] numPreStrs = new string[] { "130", "131", "132", "133", "134", "135", "136", "137", "138", "139", "150", "151", "52", "153", "154", "155", "156", "157", "158", "159", "180", "181", "182", "183", "184", "185", "186", "187", "188", "189" };
+                System.Random r =  new System.Random(System.DateTime.Now.Millisecond);
+                string username ="janpoo"+ r.Next(1, 999999).ToString() ;
+                string mobile = "138"+ r.Next(10000000, 99999999).ToString();
+                string pwd = r.Next(1000, 9999).ToString();
+                string sXmlMessage = "voteid=20&chkid=270&name=" + username + "&tel=" + mobile + "&yzmyzmm=" + pwd + "&firstecodedd=" + pwd + "&Submit3=" + pwd;//%CC%E1%BD%BB%CD%B6%C6%B1
+                //把sXmlMessage发送到指定的DsmpUrl地址上
+                Encoding encode = System.Text.Encoding.GetEncoding("gb2312");
+                byte[] arrB = encode.GetBytes(sXmlMessage);
+
+                string ip = (textBox1.Text.IndexOf(':') > 0) ? textBox1.Text.Split(':')[0] : textBox1.Text.Split(' ')[0];
+                ip = ip.Trim();
+                string port = (textBox1.Text.IndexOf(':') > 0) ? textBox1.Text.Split(':')[1] : textBox1.Text.Split(' ')[1];
+                port = port.Trim();
+                WebProxy proxyObject = new WebProxy(ip, int.Parse(port));
+                HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create("http://cmf.328f.cn/vote/vot.asp");
+                myReq.Proxy = proxyObject;
+                myReq.Method = "POST";
+                myReq.ContentType = "application/x-www-form-urlencoded";
+                myReq.ContentLength = arrB.Length;
+                myReq.Headers.Add("SOURCE", "256.256.256."+r.Next(100, 255).ToString());
+                myReq.Headers.Add("VIA", "256.256.256." + r.Next(100, 255).ToString());
+                myReq.Headers.Add("X_FORWARDED_FOR", "256.256.256." + r.Next(100, 255).ToString());
+                myReq.Headers.Add("REMOTE_ADDR", "256.256.256." + r.Next(100, 255).ToString());
+                
+                Stream outStream = myReq.GetRequestStream();
+                outStream.Write(arrB, 0, arrB.Length);
+                outStream.Close();
+
+
+                //接收HTTP做出的响应
+                WebResponse myResp = myReq.GetResponse();
+                Stream ReceiveStream = myResp.GetResponseStream();
+                StreamReader readStream = new StreamReader(ReceiveStream, encode);
+                Char[] read = new Char[256];
+                int count = readStream.Read(read, 0, 256);
+                string str = null;
+                while (count > 0)
+                {
+                    str += new String(read, 0, count);
+                    count = readStream.Read(read, 0, 256);
+                }
+                readStream.Close();
+                myResp.Close();
+                if(true)
+                {
+                    FileStream fs = new FileStream("c:\\proxy.txt", FileMode.Append);
+                    //获得字节数组
+                    byte[] data = new UTF8Encoding().GetBytes(textBox1.Text+"\r\n");
+                    //开始写入
+                    fs.Write(data, 0, data.Length);
+                    //清空缓冲区、关闭流
+                    fs.Flush();
+                    fs.Close();
+                }
+                MessageBox.Show(str);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            this.loadData();
+            int votTimes;
+            string msg = "";
+            int success = 0;
+            int fail = 0;
+            try
+            {
+                votTimes = int.Parse(textBox3.Text);
+                for (int i = 0; i < votTimes; i++)
+                {
+                    if (File.Exists("c:\\history.txt"))
+                    {
+                        this.historyArr = File.ReadAllLines("c:\\history.txt");
+                    }
+
+                    string m = getName();
+                    string p = getProxy();
+                    string ip = p.Split(':')[0];
+                    string port = p.Split(':')[1];
+                    if (m == "")
+                    {
+                        MessageBox.Show("没有设置人员名单");
+                        return;
+                    }
+
+                    if (p == "")
+                    {
+                        MessageBox.Show("没有设置代理列表");
+                        return;
+                    }
+
+                    string result = vote(m, ip, port);
+
+                   
+                    if (result.StartsWith( "投票成功"))
+                    {
+                        success++;
+                    }
+                    else
+                    {
+                        fail++;
+                    }
+
+
+                    string showmsg = m + ":" + p + ":" + result;
+                    StreamWriter sw = new StreamWriter("c:\\history.txt",true);
+                    sw.WriteLine(showmsg);
+                    sw.Flush();
+                    sw.Close();
+
+                    listBox1.Items.Add(showmsg);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add("投票出错："+ex.Message);
+            }
+
+        }
+        private void loadData()
+        {
+            if (File.Exists("c:\\proxy.txt"))
+            {
+                this.proxyArr = File.ReadAllLines("c:\\proxy.txt");
+            }
+            else
+            {
+                MessageBox.Show("c:\\proxy.txt不存在");
+                this.Close();
+            }
+            /*
+            if (File.Exists("c:\\man.txt"))
+            {
+                this.manArr = File.ReadAllLines("c:\\man.txt");
+
+            }
+            else
+            {
+                MessageBox.Show("c:\\man.txt不存在");
+                this.Close();
+            }
+
+
+            if (File.Exists("c:\\history.txt"))
+            {
+                this.historyArr = File.ReadAllLines("c:\\history.txt");
+            }
+             * */
+        }
+        private string vote(string man,string ip,string port)
+        {
+            string result = "投票成功";
+            try
+            {
+                string[] numPreStrs = new string[] { "130", "131", "132", "133", "134", "135", "136", "137", "138", "139", "150", "151", "52", "153", "154", "155", "156", "157", "158", "159", "180", "181", "182", "183", "184", "185", "186", "187", "188", "189" };
+                System.Random r = new System.Random(System.DateTime.Now.Millisecond);
+                string username = man;
+                string pre = numPreStrs[r.Next(0, numPreStrs.Length - 1)];
+                string mobile = pre + r.Next(10000000, 99999999).ToString();
+                string pwd = r.Next(1000, 9999).ToString();
+                string sXmlMessage = "voteid=20&chkid=270&name=" + username + "&tel=" + mobile + "&yzmyzmm=" + pwd + "&firstecodedd=" + pwd + "&Submit3=" + pwd;//%CC%E1%BD%BB%CD%B6%C6%B1
+               
+                
+                //把sXmlMessage发送到指定的DsmpUrl地址上
+                Encoding encode = System.Text.Encoding.GetEncoding("gb2312");
+                byte[] arrB = encode.GetBytes(sXmlMessage);
+
+                WebProxy proxyObject = new WebProxy(ip, int.Parse(port));
+                HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create("http://cmf.328f.cn/vote/vot.asp");
+                myReq.Proxy = proxyObject;
+                myReq.Method = "POST";
+                myReq.ContentType = "application/x-www-form-urlencoded";
+                myReq.ContentLength = arrB.Length;
+               /*
+                myReq.Headers.Add("SOURCE", "256.256.256." + r.Next(100, 255).ToString());
+                myReq.Headers.Add("VIA", "256.256.256." + r.Next(100, 255).ToString());
+                myReq.Headers.Add("X_FORWARDED_FOR", "256.256.256." + r.Next(100, 255).ToString());
+                myReq.Headers.Add("REMOTE_ADDR", "256.256.256." + r.Next(100, 255).ToString());
+                */
+                Stream outStream = myReq.GetRequestStream();
+                outStream.Write(arrB, 0, arrB.Length);
+                outStream.Close();
+
+
+                //接收HTTP做出的响应
+                WebResponse myResp = myReq.GetResponse();
+                Stream ReceiveStream = myResp.GetResponseStream();
+                StreamReader readStream = new StreamReader(ReceiveStream, encode);
+                Char[] read = new Char[256];
+                int count = readStream.Read(read, 0, 256);
+                string str = null;
+                while (count > 0)
+                {
+                    str += new String(read, 0, count);
+                    count = readStream.Read(read, 0, 256);
+                }
+                readStream.Close();
+                myResp.Close();
+                if (str.IndexOf("呵呵") >= 0)
+                {
+                    result = "投票失败(" + mobile + ")";
+                }
+                else
+                {
+                    result = "投票成功("+mobile+")" ;
+                }
+            }
+            catch (Exception ex)
+            {
+                result = "投票出错[" + ex.Message + "]";
+                MessageBox.Show(ex.Message);
+            }
+
+            return result;
+        }
+
+        private string getName()
+        {
+            if (historyArr == null)
+            {
+                return manArr[0].Trim();
+            }
+            bool f = true;
+            for (int i = 0; i < this.manArr.Count(); i++)
+            {
+                f = true;
+                for (int j = 0; j < this.historyArr.Count(); j++)
+                {
+                    if (historyArr[j].Trim().IndexOf(manArr[i].Trim()) >= 0)
+                    {
+                        f = false;
+                        break;
+                    }
+                }
+
+                if (f)
+                {
+                    return manArr[i].Trim();
+                }
+            }
+            return string.Empty;
+        }
+
+
+        private string getProxy()
+        {
+            if (historyArr == null)
+            {
+                return proxyArr[0].Trim();
+            }
+
+            bool f = true;
+            for (int i = 0; i < this.proxyArr.Count(); i++)
+            {
+                for (int j = 0; j < this.historyArr.Count(); j++)
+                {
+                    f = true;
+                    if (historyArr[j].Trim().IndexOf(proxyArr[i].Trim()) >= 0)
+                    {
+                        f = false;
+                        break;
+                    }
+                }
+
+                if (f)
+                {
+                    return proxyArr[i].Trim();
+                }
+            }
+            return string.Empty;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.proxyArr = File.ReadAllLines("c:\\proxy.txt");
+            for (int i = 0; i < this.proxyArr.Length; i++)
+            {
+                string p = this.proxyArr[i];
+                if (p.Trim() == "") continue;
+                string ip = p.Split(':')[0];
+                string port = p.Split(':')[1];
+                System.Threading.Thread.Sleep(100);
+                System.Threading.Thread t = new System.Threading.Thread(this.readUrl);
+                t.Start(p);
+                
+            }
+        }
+
+
+        private void readUrl(object o )
+        {
+            string p = o.ToString();
+            string ip = p.Split(':')[0];
+            string port = p.Split(':')[1];
+            string url = "http://www.19lou.com/forum-94-thread-10191379159677285-1-1.html";
+            try
+            {
+                
+
+                WebProxy proxyObject = new WebProxy(ip, int.Parse(port));
+                HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(url);
+                myReq.Proxy = proxyObject;
+                myReq.Method = "GET";
+                myReq.ContentType = "application/x-www-form-urlencoded";
+                //myReq.ContentLength = arrB.Length;
+                /*
+                 myReq.Headers.Add("SOURCE", "256.256.256." + r.Next(100, 255).ToString());
+                 myReq.Headers.Add("VIA", "256.256.256." + r.Next(100, 255).ToString());
+                 myReq.Headers.Add("X_FORWARDED_FOR", "256.256.256." + r.Next(100, 255).ToString());
+                 myReq.Headers.Add("REMOTE_ADDR", "256.256.256." + r.Next(100, 255).ToString());
+                 */
+                //Stream outStream = myReq.GetRequestStream();
+               // outStream.Write(arrB, 0, arrB.Length);
+                //outStream.Close();
+
+
+                //接收HTTP做出的响应
+                WebResponse myResp = myReq.GetResponse();
+                Stream ReceiveStream = myResp.GetResponseStream();
+                Encoding encode = System.Text.Encoding.GetEncoding("gb2312");
+                StreamReader readStream = new StreamReader(ReceiveStream, encode);
+              
+                Char[] read = new Char[256];
+                int count = readStream.Read(read, 0, 256);
+                string str = null;
+                while (count > 0)
+                {
+                    str += new String(read, 0, count);
+                    count = readStream.Read(read, 0, 256);
+                }
+                readStream.Close();
+                myResp.Close();
+               // listBox1.Items.Add(str);
+                
+            }
+            catch (Exception ex)
+            {
+                //result = "投票出错[" + ex.Message + "]";
+                MessageBox.Show(ex.Message);
+            }
+
+            //return result;
+        }
+
+
+    }
+
+
+
+
+}
